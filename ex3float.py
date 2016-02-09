194e4984d176f15260d64cf757b13fbe9c01a2e0
acirederf <frederica.lipmann@gmail.com>
2016-02-09 12:56:09 -0600

    Create ex3float.py

---
+++ ex3float.py
@@ -0,0 +1,31 @@
+#This will print the indicated string.
+print "I will now count my chickens:"
+
+print "Hens", 25.0 + 30 / 6
+print "Roosters", 100 - 25 * 3.0 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
+
+#This will print the indicated string.
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# This will evaluate the inequality below, and return "True" or "False".
+print 3.0 + 2 < 5 - 7
+
+# This will print the string, then calculate the sum of 3 and 2.
+print "What is 3 + 2?", 3.0 + 2
+# This will print the string, then calculate 5 minus 7.
+print "What is 5 - 7?", 5.0 - 7
+
+# This will print this string.
+print "Oh, that's why it's False."
+
+# And again.
+print "How about some more."
+
+#These will all print the indicated string and then calculate the inequality after the comma, and return a value "True" or "False"
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
