10ec4340141eed367a28b57f2379659a4f655b56
acirederf <frederica.lipmann@gmail.com>
2016-02-09 12:55:15 -0600

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,36 @@
+# This will print the thing it says.
+print "I will now count my chickens:"
+
+# This will print "Hens" and then calculate 25 plus 30 divided by 6
+print "Hens", 25 + 30 / 6
+# This will print "Roosters" and calculate the remainder of 100 minus the remainder of 75 divided by 4, which is 3.
+print "Roosters", 100 - 25 * 3 % 4
+
+# This will print the thing it says.
+print "Now I will count the eggs:"
+
+# This will calculate 3 plus 2 plus 1 minus 5 + 0 (the remainder of 4 divided by 2) - 0 (1 divided by 4 with the fractional bit dropped) plus 6
+# That is, 3 + 2 + 1 - 5 + 0 - 0 + 6, which is 7
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Printy printy.
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# This will evaluate the inequality below.
+print 3 + 2 < 5 - 7
+
+# This will print the string, then calculate the sum of 3 and 2.
+print "What is 3 + 2?", 3 + 2
+# This will print the string, then calculate 5 minus 7.
+print "What is 5 - 7?", 5 - 7
+
+# This will print this string.
+print "Oh, that's why it's False."
+
+# And again.
+print "How about some more."
+
+#These will all print the indicated string and then calculate the inequality after the comma, and return a value "True" or "False"
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
