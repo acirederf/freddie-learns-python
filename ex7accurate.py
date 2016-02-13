2153af80e8dd732e25ad464f464a325cf1800ec1
acirederf <frederica.lipmann@gmail.com>
2016-02-12 18:00:25 -0600

    Create ex7accurate.py

---
+++ ex7accurate.py
@@ -0,0 +1,17 @@
+#More accurate:
+
+x = "little "
+y = "lamb"
+what_she_had = x + y
+print "Mary had a %s." % what_she_had
+print "(%s)" % what_she_had
+print "(%s)" % what_she_had
+print "Mary had a %s." % what_she_had
+print "Whose fleece was white as %s." % 'snow'
+
+where = "Mary went"
+print "And everywhere that %s" % where
+print "(%s)" % where
+print "(%s)" % where
+print "Everywhere that %s" % where
+print "The %s was sure to go." % y
